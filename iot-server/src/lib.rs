46641820cc7fe1b0929042016052e8c6077c3c44
HuyDNA <huydo862003@gmail.com>
2024-02-21 00:03:16 +0700

    fix: iot_server interface

--- iot-server/src/lib.rs
+++ iot-server/src/lib.rs
@@ -1,20 +1,32 @@
-use mongodb::Client;
-use database_connector::database;
-
 mod features;
 
+use database_connector::database;
+use rumqttc::MqttOptions;
 use features::Feature;
 
+pub struct ServerConfig {
+    mongo_url: String,
+    server_id: String,
+    mqtt_url: String,
+    mqtt_cap: usize,
+}
+
 pub struct Server {
-    dbclient: Client,
+    mongo_client: mongodb::Client,
+    mqtt_client: rumqttc::Client,
+    mqtt_connection: rumqttc::Connection,
 }
 
 impl Server {
-    async fn create(database_url: String, database_name: String) -> Option<Self> {
-        match database::init(database_url, database_name).await {
-            Ok(client) => Some(Server { dbclient: client}),
+    async fn create(config: ServerConfig) -> Option<Self> {
+        match database::init(config.mongo_url).await {
+            Ok(mongo_client) => Some(mongo_client),
             _ => None
-        }
+        }.and_then( |mongo_client| {
+            let options = rumqttc::MqttOptions::parse_url(config.mqtt_url)?;
+            let (mut client, mut connection) = rumqttc::Client::new(options, config.mqtt_cap);
+            Some(Server { mongo_client, mqtt_client: client, mqtt_connection: connection })
+        })
     }
 
     async fn register(&mut self, mut module: impl Feature) -> &mut Self {
