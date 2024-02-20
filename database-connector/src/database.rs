f465bfbc802de1de8b7d6e8a96c8bdb804304478
HuyDNA <huydo862003@gmail.com>
2024-02-20 22:52:45 +0700

    fix: simplify database connector

--- database-connector/src/database.rs
+++ database-connector/src/database.rs
@@ -1,25 +1,12 @@
-use mongodb::{bson::Document, options::ClientOptions, Client, Collection};
-use dotenv::dotenv;
+use mongodb::{options::ClientOptions, Client};
 
-#[derive(Debug)]
-pub struct DB {
-    pub collection: Collection<Document>,
-}
+pub async fn init(mongodb_uri: String, database_name: String) -> mongodb::error::Result<Client> {
+    let mut client_options = ClientOptions::parse(mongodb_uri).await?;
+    client_options.app_name = Some(database_name);
 
-impl DB {
-    pub async fn init() -> mongodb::error::Result<Client> {
-        dotenv().ok();
-        let mongodb_uri: String = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
-        let database_name: String =
-            std::env::var("MONGO_INITDB_DATABASE").expect("MONGO_INITDB_DATABASE must be set.");
+    let client = Client::with_options(client_options)?;
 
-        let mut client_options = ClientOptions::parse(mongodb_uri).await?;
-        client_options.app_name = Some(database_name.to_string());
+    println!("✅ Database connected successfully");
 
-        let client = Client::with_options(client_options)?;
-
-        println!("✅ Database connected successfully");
-
-        Ok(client)
-    }
+    Ok(client)
 }
