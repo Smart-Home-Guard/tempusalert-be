cde47f4201751b5c12545aa8252718caf7b1ed67
HuyDNA <huydo862003@gmail.com>
2024-02-20 22:55:50 +0700

    fix: update server

--- iot-server/src/lib.rs
+++ iot-server/src/lib.rs
@@ -1,13 +1,23 @@
+use mongodb::Client;
+use database_connector::database;
+
 mod features;
 
 use features::Feature;
 
-struct Server {
-
+pub struct Server {
+    dbclient: Client,
 }
 
 impl Server {
-    fn register(&mut self, mut module: impl Feature) -> &mut Self {
+    async fn create(database_url: String, database_name: String) -> Option<Self> {
+        match database::init(database_url, database_name).await {
+            Ok(client) => Some(Server { dbclient: client}),
+            _ => None
+        }
+    }
+
+    async fn register(&mut self, mut module: impl Feature) -> &mut Self {
         module.init();
         self
     }
