ea276cc88a2836a28cc7872d6921e51edbbf19df
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-02-28 16:29:06 +0700

    feat(broker-server): initial mqtt broker (#4)

---
+++ src/bin/broker-server/main.rs
@@ -0,0 +1,31 @@
+use rumqttd::{Broker, Config};
+use std::{file, path::Path};
+
+#[tokio::main]
+async fn main() {
+    let this_file = file!();
+    let path = Path::new(this_file);
+    let config_path = path.parent().unwrap().join("rumqttd.toml");
+    println!("The current directory is {}", config_path.display());
+
+    let builder = tracing_subscriber::fmt()
+        .pretty()
+        .with_line_number(false)
+        .with_file(false)
+        .with_thread_ids(false)
+        .with_thread_names(false);
+
+    builder
+        .try_init()
+        .expect("initialized subscriber succesfully");
+
+    let config = config::Config::builder()
+        .add_source(config::File::with_name(config_path.to_str().unwrap()))
+        .build()
+        .unwrap();
+    let mut config: Config = config.try_deserialize().unwrap();
+
+    let mut broker = Broker::new(config);
+
+    broker.start().unwrap();
+}
