419c6c35a709d602752eede335946a3ad6073de1
quannhg <quan.nguyenhg12@gmail.com>
2024-02-28 17:17:54 +0700

    fix(mqtt_client): adjust config atributes type

--- src/mqtt_client/mod.rs
+++ src/mqtt_client/mod.rs
@@ -1,16 +1,15 @@
 use rumqttc::{AsyncClient, EventLoop, MqttOptions, QoS};
 use std::time::Duration;
 
-pub struct ClientConfig {
-    pub client_id: String,
-    pub broker_hostname: String,
+pub struct ClientConfig<'a> {
+    pub client_id: &'a str,
+    pub broker_hostname: &'a str,
     pub broker_port: u16,
     pub capacity: usize,
     pub keep_alive_sec: u64,
 }
 
-#[tokio::main]
-pub async fn init(config: ClientConfig) -> (AsyncClient, EventLoop) {
+pub fn init(config: ClientConfig) -> (AsyncClient, EventLoop) {
     let mut mqttoptions = MqttOptions::new(config.client_id, config.broker_hostname, config.broker_port);
     mqttoptions.set_keep_alive(Duration::from_secs(config.keep_alive_sec));
 
