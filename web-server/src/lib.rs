0320c4d1e0f0590abd15c695081d947aee304c00
ngyngcphu <nguyenngocphu225@gmail.com>
2024-02-28 13:04:46 +0700

    refactor(template): remove redundant module in lib

--- web-server/src/lib.rs
+++ web-server/src/lib.rs
@@ -1,11 +1,10 @@
 pub mod configs;
 pub mod constants;
 pub mod dtos;
+pub mod features;
 pub mod handlers;
 pub mod models;
 pub mod repositories;
 pub mod routes;
 pub mod types;
 pub mod utils;
-pub mod constants;
-pub mod features;
