b637c740924d112ce17be86a2fffe4ee91edaf75
HuyDNA <huydo862003@gmail.com>
2024-03-16 16:35:58 +0700

    test: add full serialzation test for device status messages

--- src/backend_core/features/device_status_feature/iot/mqtt_messages.rs
+++ src/backend_core/features/device_status_feature/iot/mqtt_messages.rs
@@ -44,7 +44,7 @@ pub struct DisconnectDeviceData {
 
 #[cfg(test)]
 mod deserialize_tests {
-    use super::{DeviceStatusMQTTMessage, ReadBatteryData};
+    use super::{DeviceStatusMQTTMessage, ReadBatteryData, ReadDeviceErrorData, DisconnectDeviceData, ConnectDeviceData};
 
     #[test]
     fn deserialize_readbattery() {
@@ -77,6 +77,151 @@ mod deserialize_tests {
         let result: DeviceStatusMQTTMessage = serde_json::from_str(input).unwrap();
         let expected = DeviceStatusMQTTMessage::ReadBattery(vec![ReadBatteryData { id: 0, value: 30 }, ReadBatteryData { id: 1, value: 50 }, ReadBatteryData { id: 2, value: 50 }, ReadBatteryData { id: 3, value: 100 }, ReadBatteryData { id: 4, value: 0 }]);
         
+        assert_eq!(result, expected);
+    }
+    
+    #[test]
+    fn deserialize_readdeviceerror() {
+        let input = r#"{
+            "kind": "1",
+            "data": [
+                {
+                    "id": 0,
+                    "component": 8 
+                }
+            ]
+        }"#;
+
+        let result: DeviceStatusMQTTMessage = serde_json::from_str(input).unwrap();
+        let expected = DeviceStatusMQTTMessage::ReadDeviceError(vec![ReadDeviceErrorData { id: 0, component: 8 }]);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn deserialize_readconnectdevice() {
+        let input = r#"{
+            "kind": "2",
+            "data": [
+                {
+                    "id": 0,
+                    "component": 1 
+                }
+            ]
+        }"#;
+
+        let result: DeviceStatusMQTTMessage = serde_json::from_str(input).unwrap();
+        let expected = DeviceStatusMQTTMessage::ConnectDevice(vec![ConnectDeviceData { id: 0, component: 1 }]);
+        
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn deserialize_readdisconnectdevice() {
+        let input = r#"{
+            "kind": "3",
+            "data": [
+                {
+                    "id": 0,
+                    "component": 1 
+                }
+            ]
+        }"#;
+
+        let result: DeviceStatusMQTTMessage = serde_json::from_str(input).unwrap();
+        let expected = DeviceStatusMQTTMessage::DisconnectDevice(vec![DisconnectDeviceData { id: 0, component: 1 }]);
+        
+        assert_eq!(result, expected);
+    }
+}
+
+#[cfg(test)]
+mod serialize_tests {
+    use super::{DeviceStatusMQTTMessage, ReadBatteryData, ReadDeviceErrorData, DisconnectDeviceData, ConnectDeviceData};
+
+    #[test]
+    fn serialize_readbattery() {
+        let input = DeviceStatusMQTTMessage::ReadBattery(vec![ReadBatteryData { id: 0, value: 30 }, ReadBatteryData { id: 1, value: 50 }, ReadBatteryData { id: 2, value: 50 }, ReadBatteryData { id: 3, value: 100 }, ReadBatteryData { id: 4, value: 0 }]);
+        let result = serde_json::to_string(&input).unwrap().parse::<serde_json::Value>().unwrap();
+        let expected = r#"{
+            "kind": "0",
+            "data": [
+                {
+                    "id": 0,
+                    "value": 30
+                },
+                {
+                    "id": 1,
+                    "value": 50
+                },
+                {
+                    "id": 2,
+                    "value": 50
+                },
+                {
+                    "id": 3,
+                    "value": 100
+                },
+                {
+                    "id": 4,
+                    "value": 0
+                }
+            ]
+        }"#.parse::<serde_json::Value>().unwrap();
+
+        assert_eq!(result, expected);
+    }
+    
+    #[test]
+    fn serialize_readdeviceerror() {
+        let input = DeviceStatusMQTTMessage::ReadDeviceError(vec![ReadDeviceErrorData { id: 0, component: 8 }]);
+        let result = serde_json::to_string(&input).unwrap().parse::<serde_json::Value>().unwrap();
+        let expected = r#"{
+            "kind": "1",
+            "data": [
+                {
+                    "id": 0,
+                    "component": 8 
+                }
+            ]
+        }"#.parse::<serde_json::Value>().unwrap();
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn serialize_readconnectdevice() {
+        let input = DeviceStatusMQTTMessage::ConnectDevice(vec![ConnectDeviceData { id: 0, component: 1 }]);
+        let result = serde_json::to_string(&input).unwrap().parse::<serde_json::Value>().unwrap();
+
+        let expected = r#"{
+            "kind": "2",
+            "data": [
+                {
+                    "id": 0,
+                    "component": 1 
+                }
+            ]
+        }"#.parse::<serde_json::Value>().unwrap();        
+        
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn serialize_readdisconnectdevice() {
+        let input = DeviceStatusMQTTMessage::DisconnectDevice(vec![DisconnectDeviceData { id: 0, component: 1 }]);
+        let result = serde_json::to_string(&input).unwrap().parse::<serde_json::Value>().unwrap();
+
+        let expected = r#"{
+            "kind": "3",
+            "data": [
+                {
+                    "id": 0,
+                    "component": 1 
+                }
+            ]
+        }"#.parse::<serde_json::Value>().unwrap();
+
         assert_eq!(result, expected);
     }
 }
