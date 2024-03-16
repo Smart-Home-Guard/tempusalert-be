c735122dbabbfd9090eb08882e31dab51f8c109b
HuyDNA <huydo862003@gmail.com>
2024-03-17 00:04:30 +0700

    fix: move IotClientClaim and WebClientClaim

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,6 +1,7 @@
 use hmac::{Hmac, Mac};
 use jwt::{FromBase64, SignWithKey, VerifyWithKey};
-use serde::Serialize;
+use mongodb::bson::doc;
+use serde::{Deserialize, Serialize};
 use sha2::Sha256;
 
 pub fn sign_jwt(key: &str, claim: &impl Serialize) -> Option<String> {
@@ -11,4 +12,35 @@ pub fn sign_jwt(key: &str, claim: &impl Serialize) -> Option<String> {
 pub fn decrypt_jwt<T: FromBase64>(key: &str, token_str: &str) -> Option<T> {
     let key: Hmac<Sha256> = Hmac::new_from_slice(key.as_bytes()).ok()?;
     token_str.verify_with_key(&key).ok()
+}
+
+pub fn get_client_id_from_token(key: &str, token: String) -> Option<String> {
+    let claim = decrypt_jwt::<IotClientClaim>(key, token.as_str())?;
+    Some(claim.client_id)
+}
+
+pub async fn get_username_from_token(
+    key: &str,
+    token: String,
+    mongoc: mongodb::Client,
+) -> Option<String> {
+    let claim = decrypt_jwt::<IotClientClaim>(key, token.as_str())?;
+    mongoc
+        .default_database()?
+        .collection("users")
+        .find_one(doc! { "client_id": claim.client_id }, None)
+        .await
+        .ok()?
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct IotClientClaim {
+    pub client_id: String,
+    pub nonce: String,
+}
+
+#[derive(Serialize)]
+pub struct WebClientClaim {
+    pub username: String,
+    pub nonce: String,
 }
--- src/bin/backend/web/utils.rs
+++ src/bin/backend/web/utils.rs
@@ -1,16 +1,10 @@
 use std::num::NonZeroU32;
 
-use mongodb::bson::doc;
 use once_cell::sync::Lazy;
 use ring::{
     digest, pbkdf2,
     rand::{self, SecureRandom},
 };
-use tempusalert_be::auth::decrypt_jwt;
-
-use crate::config::JWT_KEY;
-
-use super::auth_apis::IotClientClaim;
 
 const N_ITER: Lazy<NonZeroU32> = Lazy::new(|| NonZeroU32::new(100_000).unwrap());
 
@@ -51,14 +45,4 @@ pub fn verify_hashed_password(
         &hashed_password,
     )
     .is_ok()
-}
-
-pub fn get_client_id_from_token(token: String) -> Option<String> {
-    let claim = decrypt_jwt::<IotClientClaim>(JWT_KEY.as_str(), token.as_str())?;
-    Some(claim.client_id)
-}
-
-pub async fn get_username_from_token(token: String, mongoc: mongodb::Client) -> Option<String> {
-    let claim = decrypt_jwt::<IotClientClaim>(JWT_KEY.as_str(), token.as_str())?;
-    mongoc.default_database()?.collection("users").find_one(doc! { "client_id": claim.client_id }, None).await.ok()?
 }
