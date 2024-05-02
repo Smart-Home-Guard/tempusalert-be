8f1df5b7179eadde8d65ff65811afa2d83b3a04a
Huy-DNA <huydo862003@gmail.com>
2024-05-02 10:34:43 +0700

    chore: basic setup for remote_control_feature

---
+++ src/backend_core/features/remote_control_feature/mod.rs
@@ -0,0 +1,10 @@
+mod iot;
+mod models;
+mod notifications;
+mod web;
+
+pub use iot::IotDeviceStatusFeature as IotFeature;
+pub use notifications::DeviceStatusIotNotification as IotNotification;
+pub use notifications::DeviceStatusWebNotification as WebNotification;
+pub use web::WebDeviceStatusFeature as WebFeature;
+pub static MUST_ON: bool = true;
