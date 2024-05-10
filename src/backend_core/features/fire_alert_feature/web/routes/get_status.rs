be2c351c32c499b48e0d2f0a03fe2cade461ac63
Huy-DNA <huydo862003@gmail.com>
2024-05-11 00:05:30 +0700

    feat: add log query by room api

--- src/backend_core/features/fire_alert_feature/web/routes/get_status.rs
+++ src/backend_core/features/fire_alert_feature/web/routes/get_status.rs
@@ -1,7 +1,7 @@
 use aide::axum::{routing::get_with, ApiRouter, IntoApiResponse};
 use axum::{
     extract::Query,
-    http::{HeaderMap, StatusCode},
+    http::{HeaderMap, StatusCode}, Json,
 };
 use mongodb::{
     bson::{self, doc},
@@ -10,10 +10,7 @@ use mongodb::{
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use crate::{
-    backend_core::{features::{devices_status_feature::models::Device, fire_alert_feature::models::{FireLog, FireStatus}}, models::Room},
-    json::Json,
-};
+use crate::backend_core::{features::{devices_status_feature::models::Device, fire_alert_feature::models::{FireLog, FireStatus, SensorDataType}}, models::Room};
 
 use super::MONGOC;
 
@@ -22,12 +19,28 @@ pub struct GetStatusQuery {
     email: String,
     component_ids: Option<Vec<String>>,
     room_name: Option<String>,
+    co: Option<()>,
+    fire: Option<()>,
+    gas: Option<()>,
+    heat: Option<()>,
+    light: Option<()>,
+    button: Option<()>,
+    buzzer: Option<()>,
+    smoke: Option<()>,
+    start_time: Option<i32>,
+    end_time: Option<i32>,
 }
 
 #[derive(Serialize, JsonSchema)]
 pub struct ComponentSafetyStatus {
     id: usize,
     status: FireStatus,
+}
+
+#[derive(Deserialize, Serialize, JsonSchema)]
+pub struct RoomLogEntryResponse {
+    timestamp: usize,
+    value: isize,
 }
 
 #[derive(Serialize, JsonSchema)]
@@ -40,6 +53,18 @@ pub enum GetStatusResponse {
     RoomSafetyStatus {
         message: String,
         value: Option<Vec<ComponentSafetyStatus>>,
+    },
+    RoomDetailedSafetyStatus {
+        message: String,
+        component_statuses: Option<Vec<ComponentSafetyStatus>>,
+        co_logs: Option<Vec<RoomLogEntryResponse>>,
+        heat_logs: Option<Vec<RoomLogEntryResponse>>,
+        smoke_logs: Option<Vec<RoomLogEntryResponse>>,
+        light_logs: Option<Vec<RoomLogEntryResponse>>,
+        buzzer_logs: Option<Vec<RoomLogEntryResponse>>,
+        button_logs: Option<Vec<RoomLogEntryResponse>>,
+        fire_logs: Option<Vec<RoomLogEntryResponse>>,
+        gas_logs: Option<Vec<RoomLogEntryResponse>>,
     },
     Unrecognized {
         message: String,
@@ -59,10 +84,34 @@ async fn handler(
         email,
         component_ids,
         room_name,
+        co,
+        fire,
+        gas,
+        heat,
+        light,
+        button,
+        buzzer,
+        smoke,
+        start_time,
+        end_time,
     }): Query<GetStatusQuery>,
 ) -> impl IntoApiResponse {
     if room_name.is_some() {
-        handle_room_status(headers, email, room_name.unwrap()).await
+        if vec![co, fire, gas, heat, light, button, buzzer, smoke].into_iter().all(|opt| opt.is_none()) {
+            handle_room_status(headers, email, room_name.unwrap()).await
+        } else {
+            let mut types = vec![];
+            co.map(|_| types.push(SensorDataType::CO));
+            fire.map(|_| types.push(SensorDataType::Fire));
+            gas.map(|_| types.push(SensorDataType::LPG));
+            heat.map(|_| types.push(SensorDataType::Heat));
+            light.map(|_| types.push(SensorDataType::FireLight));
+            button.map(|_| types.push(SensorDataType::FireButton));
+            button.map(|_| types.push(SensorDataType::FireBuzzer));
+            smoke.map(|_| types.push(SensorDataType::Smoke));
+
+            handle_room_status_of_types(headers, email, room_name.unwrap(), types, start_time, end_time).await
+        }
     } else if component_ids.is_some() {
         handle_component_statuses(headers, email, component_ids.unwrap()).await
     } else {
@@ -74,6 +123,140 @@ async fn handler(
             }),
         )
     }
+}
+
+async fn handle_room_status_of_types(
+    headers: HeaderMap,
+    email: String,
+    room_name: String,
+    types: Vec<SensorDataType>,
+    start_time: Option<i32>,
+    end_time: Option<i32>,
+) -> (StatusCode, Json<GetStatusResponse>) {
+    let response = handle_room_status(headers, email.clone(), room_name.clone()).await;
+    if let GetStatusResponse::RoomSafetyStatus { message, value } = response.1.0 {
+        if value.is_none() {
+            return (
+                response.0,
+                Json(GetStatusResponse::RoomDetailedSafetyStatus {
+                    message,
+                    component_statuses: None,
+                    co_logs: None,
+                    heat_logs: None,
+                    smoke_logs: None,
+                    light_logs: None,
+                    buzzer_logs: None,
+                    button_logs: None,
+                    fire_logs: None,
+                    gas_logs: None,
+                }),
+            );
+        }
+        
+        let mut co_logs = None;
+        let mut heat_logs = None;
+        let mut smoke_logs = None;
+        let mut light_logs = None;
+        let mut buzzer_logs = None;
+        let mut button_logs = None;
+        let mut fire_logs = None;
+        let mut gas_logs = None;
+
+        let fire_coll: Collection<FireLog> = {
+            let mongoc = unsafe { MONGOC.as_ref().clone().unwrap().lock() }.await;
+            mongoc.default_database().unwrap().collection("fire_alerts")
+        };
+
+        let component_ids = get_component_ids_by_room(email.clone(), room_name).await;
+
+        for typ in types {
+            let log_field = match typ {
+                SensorDataType::CO => "co_logs",
+                SensorDataType::LPG => "gas_logs",
+                SensorDataType::Fire => "fire_logs",
+                SensorDataType::Heat => "heat_logs",
+                SensorDataType::Smoke => "smoke_logs",
+                SensorDataType::FireLight => "light_logs",
+                SensorDataType::FireButton => "button_logs",
+                SensorDataType::FireBuzzer => "buzzer_logs",
+            };
+
+            let pipeline = vec![ 
+                doc! { "$match": { "owner_name": email.clone() } },
+                doc! { "$project": {
+                    "_id": 0,
+                    log_field: 1,
+                } },
+                doc! {
+                    "$project": {
+                        "logs": {
+                            "$concatArrays": [format!("${log_field}")],
+                        }
+                    }
+                },
+                doc! {
+                    "$replaceRoot": {
+                        "newRoot": "$logs",
+                    }
+                },
+                doc! {
+                    "$match": {
+                        "component": { "$in": &component_ids },
+                    }
+                },
+                doc! {
+                    "$sort": {
+                        "timestamp.secs_since_epoch": 1,
+                        "timestamp.nanos_since_epoch": 1,
+                    }
+                },
+                doc! {
+                    "$match": {
+                        "$and": [
+                            { "timestamp.secs_since_epoch": { "$gte": start_time.unwrap_or(0) } },
+                            { "timestamp.secs_since_epoch": { "$lte": end_time.unwrap_or(i32::MAX) } },
+                        ]
+                    }
+                },
+            ];
+            
+            let mut cursor = fire_coll.aggregate(pipeline, None).await.unwrap();
+
+            let mut res = vec![];
+            while let Ok(true) = cursor.advance().await {
+                let document = cursor.deserialize_current().unwrap();
+                res.push(bson::from_bson::<RoomLogEntryResponse>(document.into()).unwrap());
+            }
+            
+            match typ {
+                SensorDataType::CO => { co_logs = Some(res); },
+                SensorDataType::LPG => { gas_logs = Some(res); },
+                SensorDataType::Fire => { fire_logs = Some(res); },
+                SensorDataType::Heat => { heat_logs = Some(res); },
+                SensorDataType::Smoke => { smoke_logs = Some(res); },
+                SensorDataType::FireLight => { light_logs = Some(res); },
+                SensorDataType::FireButton => { button_logs = Some(res); },
+                SensorDataType::FireBuzzer => { buzzer_logs = Some(res); },
+            };
+        }
+
+        return (
+            StatusCode::OK,
+            Json(GetStatusResponse::RoomDetailedSafetyStatus {
+                message,
+                component_statuses: value,
+                co_logs,
+                heat_logs,
+                smoke_logs,
+                light_logs,
+                buzzer_logs,
+                button_logs,
+                fire_logs,
+                gas_logs,
+            }),
+        );
+    }
+    panic!("Unreachable");
 }
 
 async fn handle_room_status(
@@ -261,7 +444,7 @@ async fn get_component_statuses(email: String, component_ids: Vec<String>) -> Op
                 "_id": "$component",
                 "alert": { "$first": "$alert" },
             }
-        }, 
+        },
     ];
 
     let mut cursor = fire_coll.aggregate(pipeline, None).await.ok()?;
